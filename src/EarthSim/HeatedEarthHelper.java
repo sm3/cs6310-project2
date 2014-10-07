10609c91c2a5436f3e8459ec154762f98497b467
sm3 <subha.mela@gatech.edu>
2014-10-07 13:28:09 -0700

    second commit

--- src/EarthSim/HeatedEarthHelper.java
+++ src/EarthSim/HeatedEarthHelper.java
@@ -10,13 +10,37 @@ package EarthSim;
 public class HeatedEarthHelper
 {
 	
-	private int gridSpacing; //1 to 180
-	private int timeInterval=1; //1 to 1440
+	private int gridSpacing; //1 to 180- input provided by the user
+	private int timeInterval=1; //1 to 1440 - input provided by the user
 	private static int eradius = 6371000; // in meters
 	private static double esurfaceArea = 5.10072E14;
 	private static double ecircumference =40030140;
 	private static double eAreaVisibleToSun = 2.55036E14;
 	
+	public int getGridSpacing()
+	{
+		//return largest integer input provided that evenly divides 180 degrees without any reminder.
+		
+		return 0;
+		
+	}
+	
+	//add method to get grid proportion to the equator
+	
+	public int getCols(int gs)
+	{
+		return 180/gs;
+	}
+	
+	public int getRows(int gs)
+	{
+		return 180/gs;
+	}
+	
+	public int getNumberOfCells(int gs)
+	{
+	   return getRows(gs)*getCols(gs);
+	}
 	
 	
 	
--- src/EarthSim/HeatedEarthSimulation.java
+++ src/EarthSim/HeatedEarthSimulation.java
@@ -4,4 +4,15 @@ package EarthSim;
 public class HeatedEarthSimulation
 {
 	
+	
+	
+	//create an array of rowsXcols of GridCells.
+	
+	GridCell[][] gridcells = new GridCell[6][4]; //replace 6 and 4 with actual rows and cols
+	
+	public GridCell getNorth(int i, int j)
+	{
+		return null; //replace with the information provided in the domain model
+	}
+	
 }
